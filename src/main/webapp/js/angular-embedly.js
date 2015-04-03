5f241598c12f548eacb3e5867b0a844302c0d5ca
Robert Lyons <robertlyons@Roberts-MacBook-Pro.local>
2015-04-02 21:00:13 -0400

    Update all the SSL

--- src/main/webapp/js/angular-embedly.js
+++ src/main/webapp/js/angular-embedly.js
@@ -18,12 +18,12 @@ var angularEmbedly = angular.module('angular-embedly', []);
         function embedly($http) {
             this.embed = function(inputUrl) {
                 var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = 'http://api.embed.ly/1/oembed?key=' + key + '&url=' +  escapedUrl;
+                var embedlyRequest = '//api.embed.ly/1/oembed?key=' + key + '&url=' +  escapedUrl;
                 return $http({method: 'GET', url: embedlyRequest});
             };
             this.extract = function(inputUrl, maxWidth, maxHeight) {
                 var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = 'http://api.embed.ly/1/extract?key=' + key + '&url=' +  escapedUrl;
+                var embedlyRequest = '//api.embed.ly/1/extract?key=' + key + '&url=' +  escapedUrl;
 
                 if (maxWidth > 0 && maxHeight > 0) {
                     embedlyRequest = embedlyRequest + "&image_width=" + maxWidth + "&image_height=" + maxHeight + "&errorUrl=http%3A%2F%2Fmedia.tumblr.com%2Ftumblr_m9e0vfpA7K1qkbsaa.jpg&image_grow=true&image_method=resize";
